75c193810390d5c7892713dacd5359c937c632c9
DiimsFH <dimasfachri133@gmail.com>
2025-06-26 11:25:15 +0700

    Add files via upload

---
+++ 24062025/DataRekening.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum24062025;
+
+/**
+ *
+ * @author aanomali
+ */
+public class DataRekening {
+
+    public static Rekening rek1 = new Rekening("2401083004", "1234", 1000);
+    public static Rekening rek2 = new Rekening("2401083001", "5678", 5000);
+    public static Rekening aktif=null;
+}
